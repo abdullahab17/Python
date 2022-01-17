f95cd406860b4266055d74ac16bc63584c8c2f9c
Abdullah Liaqat <45213685+abdullahab17@users.noreply.github.com>
2022-01-17 18:01:34 +0500

    Add files via upload

---
+++ Messi statistics using mlt.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+print("Messi Goal Statistics through out the year")
+year = [2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
+goals = [0,3,12,31,22,41,60,59,91,45,58,52,59,54,51,50,27,43]
+
+plt.plot(year,goals)
+plt.show()

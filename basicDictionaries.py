2f851bfa24b1c8b91426293d1ed31a31efc8dc97
Abdullah Liaqat <45213685+abdullahab17@users.noreply.github.com>
2022-01-18 16:27:13 +0500

    Add files via upload

---
+++ basicDictionaries.py
@@ -0,0 +1,11 @@
+countries = ["Spain","France","Germany","Norway"]
+capitals = ["Madrid","Paris","Berlin","Oslo"]
+
+print(countries.index("Germany"))
+ind_ger = capitals[2]
+print(ind_ger)
+
+europe = {"Spain":"Madrid","France":"Paris","Germany":"Berlin","Norway":"Oslo"}
+print(europe)
+print(europe.keys())
+print(europe["Norway"])

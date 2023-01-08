8bf7d716c7c27e85652512e5468c661798412128
Abdullah Liaqat <45213685+abdullahab17@users.noreply.github.com>
2023-01-08 21:48:56 +0500

    Datacamp

---
+++ Python_Data_Science_Toolbox(Chap_3).py
@@ -0,0 +1,212 @@
+raise_to_power = lambda x,y : x ** y
+print(raise_to_power(2,3))
+
+nums = [8,6,9,4]
+square_all = map(lambda num: num**2, nums)
+print(list(square_all))
+
+print("-------------------------------------------------------")
+# Define echo_word as a lambda function: echo_word
+echo_word = (lambda word1,echo : word1*echo)
+
+# Call echo_word: result
+result = echo_word("hey",5)
+
+# Print result
+print(result)
+
+print("-------------------------------------------------------")
+# Create a list of strings: spells
+spells = ["protego", "accio", "expecto patronum", "legilimens"]
+
+# Use map() to apply a lambda function over spells: shout_spells
+shout_spells = map(lambda item: item + "!!!", spells)
+
+# Convert shout_spells to a list: shout_spells_list
+shout_spells_list = list(shout_spells)
+
+# Print the result
+print(shout_spells_list)
+
+print("-------------------------------------------------------")
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'pippin', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf']
+
+# Use filter() to apply a lambda function over fellowship: result
+result = filter(lambda member:len(member)>6, fellowship)
+
+# Convert result to a list: result_list
+result_list = list(result)
+
+# Print result_list
+print(result_list)
+
+
+print("-------------------------------------------------------")
+# Import reduce from functools
+from functools import reduce
+
+# Create a list of strings: stark
+stark = ['robb', 'sansa', 'arya', 'brandon', 'rickon']
+
+# Use reduce() to apply a lambda function over stark: result
+result = reduce(lambda item1, item2: item1 + item2,stark)
+
+# Print the result
+print(result)
+print("-------------------------------------------------------")
+
+def square(x):
+    if x < 0:
+        raise ValueError("X must be greater than 0")
+    try:
+        return x ** 2
+    except TypeError:
+        print("X must be either int or float")
+square(5)
+print("-------------------------------------------------------")
+
+
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Initialize empty strings: echo_word, shout_words
+    echo_word = ""
+    shout_words = ""
+
+    # Add exception handling with try-except
+    try:
+        # Concatenate echo copies of word1 using *: echo_word
+        echo_word = word1 * echo
+
+        # Concatenate '!!!' to echo_word: shout_words
+        shout_words = echo_word + "!!!"
+    except:
+        # Print error message
+        print("word1 must be a string and echo must be an integer.")
+
+    # Return shout_words
+    return shout_words
+
+
+# Call shout_echo
+shout_echo("particle", echo="accelerator")
+print("-------------------------------------------------------")
+
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Raise an error with raise
+    if echo < 0:
+        raise ValueError("echo must be greater than or equal to 0")
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo
+shout_echo("particle", echo=5)
+
+print("-------------------------------------------------------")
+import pandas as pd
+tweets = 'https://assets.datacamp.com/production/course_1532/datasets/tweets.csv'
+tweets_df = pd.read_csv(tweets)
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda tweets_df: tweets_df[0:2] == "RT",tweets_df["text"])
+
+# Create list from filter object result: res_list
+res_list = list(result)
+
+# Print all retweets in res_list
+for tweet in res_list:
+    print(tweet)
+
+print("-------------------------------------------------------")
+
+
+# Define count_entries()
+def count_entries(df, col_name='lang'):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+
+    # Add try block
+    try:
+        # Extract column from DataFrame: col
+        col = df[col_name]
+
+        # Iterate over the column in DataFrame
+        for entry in col:
+
+            # If entry is in cols_count, add 1
+            if entry in cols_count.keys():
+                cols_count[entry] += 1
+            # Else add the entry to cols_count, set the value to 1
+            else:
+                cols_count[entry] = 1
+
+        # Return the cols_count dictionary
+        return cols_count
+
+    # Add except block
+    except:
+        print('The DataFrame does not have a ' + col_name + ' column.')
+
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, 'lang')
+
+# Print result1
+print(result1)
+print("-------------------------------------------------------")
+
+
+# Define count_entries()
+def count_entries(df, col_name='lang'):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Raise a ValueError if col_name is NOT in DataFrame
+    if col_name not in df.columns:
+        raise ValueError('The DataFrame does not have a ' + col_name + ' column.')
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+
+    # Extract column from DataFrame: col
+    col = df[col_name]
+
+    # Iterate over the column in DataFrame
+    for entry in col:
+
+        # If entry is in cols_count, add 1
+        if entry in cols_count.keys():
+            cols_count[entry] += 1
+            # Else add the entry to cols_count, set the value to 1
+        else:
+            cols_count[entry] = 1
+
+        # Return the cols_count dictionary
+    return cols_count
+
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, 'lang')
+
+# Print result1
+print(result1)
+print("-------------------------------------------------------")
+
+
+print("-------------------------------------------------------")
